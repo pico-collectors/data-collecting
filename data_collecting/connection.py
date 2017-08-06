ede7ffd6fc2fd08ae7fa012d05429794115f384a
David Fialho <fialho.david@gmail.com>
2017-08-06 20:28:41 +0100

    Changed connection.py and protocol.py to use PEP-8

--- data_collecting/connection.py
+++ data_collecting/connection.py
@@ -1,21 +1,29 @@
 
 class Connection:
-    """ Adapter for a socket connection that provides simple methods to send and receive data """
+    """
+    Adapter for a socket connection that provides simple methods to send
+    and receive data.
+    """
 
     def __init__(self, socket_connection, address):
         """
-        Initializes a new connection adapter for the specified socket connection. This initializer should not be
-        called directly. Instead, the 'connect' function should be used to create connection objects.
+        Initializes a new connection adapter for the specified socket
+        connection. This initializer should not be called directly. Instead,
+        the 'connect' function should be used to create connection objects.
 
         :param socket_connection: the socket connection to be adapted
-        :param address: the address of the 'server' the connection was established with
+        :param address: the address of the 'server' the connection was
+                        established with
         """
         self._socket_connection = socket_connection
         self._destination_address = address
 
     @property
     def destination_address(self):
-        """ Returns the address of the server with which the connection was established """
+        """
+        Returns the address of the server with which the connection was
+        established.
+        """
         return self._destination_address
 
     def __enter__(self):
@@ -25,7 +33,10 @@ class Connection:
         self.close()
 
     def receive(self, end='\r\n') -> bytes:
-        """ Blocks to receive data from the connection until it finds the 'end' string """
+        """
+        Blocks to receive data from the connection until it finds the
+        'end' string.
+        """
         pass
 
     def send(self, message: bytes):
--- data_collecting/protocol.py
+++ data_collecting/protocol.py
@@ -1,15 +1,27 @@
 from data_collecting.connection import Connection
 
 
+class CorruptedDataError(Exception):
+    """
+    Raised by the Protocol when the data received is corrupted or the
+    format is not correct.
+    """
+    pass
+
+
 class Protocol:
-    """ The base class to describe the protocol to communicate with the data producer """
+    """
+    The base class to describe the protocol to communicate with the data
+    producer.
+    """
 
     @property
     def end_marker(self):
         """
-        Returns the marker that marks the end of each data stream expected by this protocol. Subclasses should
-        override this method to adjust for they supported marker. If the subclass does not override this property
-        getter it assumes the marker is '\r\n' by default.
+        Returns the marker that marks the end of each data stream expected by
+        this protocol. Subclasses should override this method to adjust for
+        they supported marker. If the subclass does not override this
+        property getter it assumes the marker is '\r\n' by default.
         """
         return '\r\n'
 
@@ -19,7 +31,8 @@ class Protocol:
 
     def on_data_received(self, connection: Connection, data: bytes):
         """
-        Invoked by the DataCollector once a new stream of data is received. The data parameter contains the data
-        stream that was obtained by the data collector. It does NOT contain the end marker.
+        Invoked by the DataCollector once a new stream of data is received.
+        The data parameter contains the data stream that was obtained by the
+        data collector. It does NOT contain the end marker.
         """
         pass
